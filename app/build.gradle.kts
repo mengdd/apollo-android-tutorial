861cef9e2b5d610cf83af29f186fdee3be43ab73
Dandan Meng <mengdandanno1@163.com>
2020-06-16 18:00:44 +0800

    configure the Apollo plugin to generate Kotlin files instead of Java files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,3 +51,7 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.1")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.2.0")
 }
+
+apollo {
+    generateKotlinModels.set(true)
+}
