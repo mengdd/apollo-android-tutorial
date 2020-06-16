5facef9e97deca102cb62ad2b86dd8b23d1d3fed
Dandan Meng <mengdandanno1@163.com>
2020-06-16 18:42:23 +0800

    show query results on UI

--- app/src/main/java/com/example/rocketreserver/LaunchListAdapter.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchListAdapter.kt
@@ -5,20 +5,22 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.example.rocketreserver.databinding.LaunchItemBinding
 
-class LaunchListAdapter() :
+class LaunchListAdapter(val launches: List<LaunchListQuery.Launch>) :
     RecyclerView.Adapter<LaunchListAdapter.ViewHolder>() {
 
     class ViewHolder(val binding: LaunchItemBinding) : RecyclerView.ViewHolder(binding.root)
-
-    override fun getItemCount(): Int {
-        return 0
-    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val binding = LaunchItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
         return ViewHolder(binding)
     }
 
+    override fun getItemCount(): Int {
+        return launches.size
+    }
+
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val launch = launches.get(position)
+        holder.binding.site.text = launch.site ?: ""
     }
 }
--- app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
+++ app/src/main/java/com/example/rocketreserver/LaunchListFragment.kt
@@ -7,7 +7,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.apollographql.apollo.coroutines.toDeferred
+import com.apollographql.apollo.exception.ApolloException
 import com.example.rocketreserver.databinding.LaunchListFragmentBinding
 
 class LaunchListFragment : Fragment() {
@@ -26,9 +28,19 @@ class LaunchListFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         lifecycleScope.launchWhenResumed {
-            val response = apolloClient.query(LaunchListQuery()).toDeferred().await()
+            val response = try {
+                apolloClient.query(LaunchListQuery()).toDeferred().await()
+            } catch (e: ApolloException) {
+                Log.d("LaunchList", "Failure", e)
+                null
+            }
 
-            Log.d("LaunchList", "Success ${response?.data}")
+            val launches = response?.data?.launches?.launches?.filterNotNull()
+            if (launches != null && !response.hasErrors()) {
+                val adapter = LaunchListAdapter(launches)
+                binding.launches.layoutManager = LinearLayoutManager(requireContext())
+                binding.launches.adapter = adapter
+            }
         }
     }
 }
